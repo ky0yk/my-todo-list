0b4ea56003f65496f3fb32cf2f6ae654442411b2
ky0yk <ksaik1024@gmail.com>
2021-10-15 21:18:50 +0900

    LambdaとDynamoDBを実装

--- lib/my-todo-list-stack.ts
+++ lib/my-todo-list-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import { ResourceName } from './resourceName';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
 
 export class MyTodoListStack extends cdk.Stack {
   constructor(
@@ -10,9 +12,25 @@ export class MyTodoListStack extends cdk.Stack {
     const id = resourceName.stackName('App');
     super(scope, id, props);
 
-    // Lambda
-
     // Dynamo DB
+    const todoTableName = resourceName.dynamodbName('todo');
+    const todoTable = new dynamodb.Table(this, todoTableName, {
+      tableName: todoTableName,
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'todoId', type: dynamodb.AttributeType.STRING },
+    });
+
+    // Lambda
+    const todoFunctionName = resourceName.lambdaName('todo');
+    const todoFunction = new NodejsFunction(this, todoFunctionName, {
+      functionName: todoFunctionName,
+      entry: 'src/lambda/handlers/index.ts',
+      environment: {
+        TABLE_NAME: todoTableName,
+      },
+    });
+
+    todoTable.grantReadWriteData(todoFunction);
 
     // API Gateway
 
